0c00e2b24eca280e58c3343c2f9f3d66eb527a0d
ArhamSaif <arhamsaif65@gmail.com>
2024-09-13 15:10:00 +0500

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -91,7 +91,7 @@ const popups = [
 function Hero() {
   const { ref, inView } = useInView({
     triggerOnce: false,
-    threshold: 0.1,
+    threshold: 0.5,
   });
   const [currentIndex, setCurrentIndex] = useState(0);
   const [popup, setPopup] = useState(null);
@@ -125,28 +125,29 @@ function Hero() {
         className="absolute inset-0 w-full h-full object-cover"
         alt={images[currentIndex].alt}
       />
-      <motion.div
-        ref={ref}
-        initial={{ y: -30, opacity: 0 }}
-        animate={{ y: inView ? 0 : 100, opacity: inView ? 1 : 0 }}
-        transition={{ duration: 1.2, ease: 'easeOut' }}
-        className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-70 p-6 text-center"
-      >
-        <h1 className="text-white text-3xl md:text-4xl lg:text-6xl font-bold mb-2 transition-transform duration-500 transform hover:scale-105">
-          {images[currentIndex].text}
-        </h1>
-        <p className="text-gray-300 text-base md:text-lg lg:text-xl mb-4 mx-4 md:mx-10 lg:mx-20 transition-opacity duration-500">
-          {images[currentIndex].subtitle}
-        </p>
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
-          <button className="bg-gradient-to-r from-[#098487] to-[#00B4A1] text-white text-sm md:text-lg font-semibold border-2 border-transparent rounded-full px-6 md:px-8 py-2 md:py-3 shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-white hover:text-[#e9fcfc] hover:border-[#098487] hover:shadow-xl">
-            {images[currentIndex].buttonText1}
-          </button>
-          <button className="hidden md:block bg-white text-[#098487] text-sm md:text-lg font-semibold border-2 border-[#098487] rounded-full px-6 md:px-8 py-2 md:py-3 shadow-md transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-[#098487] hover:text-white hover:shadow-xl">
-            {images[currentIndex].buttonText2}
-          </button>
-        </div>
-      </motion.div>
+     <motion.div
+  ref={ref}
+  initial={{ y: -30, opacity: 0 }}
+  animate={{ y: inView ? 0 : 100, opacity: inView ? 1 : 0 }}
+  transition={{ duration: 1.2, ease: 'easeOut' }}
+  className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-70 p-6 text-center"
+>
+  <h1 className="text-white text-3xl md:text-4xl lg:text-6xl font-bold mb-2 transition-transform duration-500 transform hover:scale-105">
+    {images[currentIndex].text}
+  </h1>
+  <p className="text-gray-300 text-base md:text-lg lg:text-xl mb-4 mx-4 md:mx-10 lg:mx-20 transition-opacity duration-500">
+    {images[currentIndex].subtitle}
+  </p>
+  <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
+    <button className="bg-gradient-to-r from-[#098487] to-[#00B4A1] text-white text-sm md:text-lg font-semibold border-2 border-transparent rounded-full px-6 md:px-8 py-2 md:py-3 shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-white hover:text-[#e9fcfc] hover:border-[#098487] hover:shadow-xl">
+      {images[currentIndex].buttonText1}
+    </button>
+    <button className="hidden md:block bg-white text-[#098487] text-sm md:text-lg font-semibold border-2 border-[#098487] rounded-full px-6 md:px-8 py-2 md:py-3 shadow-md transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-[#098487] hover:text-white hover:shadow-xl">
+      {images[currentIndex].buttonText2}
+    </button>
+  </div>
+</motion.div>
+
 
       <div className="absolute bottom-5 right-5 flex space-x-7">
         <button onClick={handlePrev} className="text-white text-lg md:text-xl lg:text-2xl shadow-md transition-transform transform hover:scale-105">
